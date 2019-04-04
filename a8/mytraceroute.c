cdfcce1f9f1aabdbb8bc1fc16eb8b7646c79236c
Anusha Potnuru <anusha.sparkx@gmail.com>
2019-04-04 11:08:33 +0530

    set headers

--- a8/mytraceroute.c
+++ a8/mytraceroute.c
@@ -1,18 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <linux/udp.h> /* for ipv4 header */ 
-#include <linux/ip.h> /* for upd header */ 
+#include <linux/udp.h> /* for udp header */ 
+#include <linux/ip.h> /* for ipv4 header */ 
+#include <linux/icmp.h> /* for icmp header */ 
+
 #include <unistd.h>
 #include <string.h> 
 #include <signal.h> 
 #include <sys/wait.h>
 #include <netdb.h>
 
-extern int h_errno;
+extern int errno;
 
+#DEFINE T_SEC 1
+#DEFINE T_USEC 0
 /*
 struct iphdr {
 #if defined(__LITTLE_ENDIAN_BITFIELD)
@@ -44,9 +48,17 @@ __u16 	len
 __u16 	check
 
 
+
+struct sockaddr_in {
+               sa_family_t    sin_family; /* address family: AF_INET
+               in_port_t      sin_port;   /* port in network byte order 
+               struct in_addr sin_addr;   /* internet address 
+           };
+
 */
 int main()
 {
+	struct in_addr dest_ip_addr;
 	char domain_name[200];
 	scanf("%s", domain_name);
 
@@ -61,13 +73,11 @@ int main()
 		{
 			printf("%s\n",  inet_ntoa(*((struct in_addr*)x->h_addr_list[i])) );
 			strcpy(ip, inet_ntoa(*((struct in_addr*)x->h_addr_list[i])));
-			int j = sendto(udpfd, (const char*)ip, strlen(ip)+1, 0, 
-						(struct sockaddr*)&cliaddr, sizeof(cliaddr));
-			// printf("%d\n",j );
-			if(j<0)
-				perror("udp send error"); 
+			memcpy(&dest_ip_addr, x->h_addr_list[i], sizeof(struct in_addr));
+
 		}
 	}
+
 	struct sockaddr_in saddr_udp, saddr_icmp;
 	int saddr_len_udp, saddr_len_icmp;
 	int udphdrlen = sizeof(hdrudp);
@@ -110,49 +120,100 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	struct iphdr *hdrip;
+	struct iphdr *hdrip, *icmp_hdrip;
 	struct updhdr *hdrudp;
+	struct icmphdr *hdricmp;
 
-	int ttl=1,r;
+	struct sockaddr_in destaddr, checkaddr;
+	destaddr.sin_family = AF_INET;
+	destaddr.sin_port = 32164; //given
+	destaddr.sin_addr =  dest_ip_addr; /* address in network byte order */
+
+	int ttl=1,r, ret, len;
 	char buffer[sizeof(struct iphdr)+sizeof(struct updhdr)+52];
 	char mesg[52];
+	fd_set rset;
+	clock_t t;
+	double time_taken;
 
 	hdrip = (struct iphdr*)buffer;
 	hdrudp = (struct updhdr*)(buffer+sizeof(struct iphdr));
 	// IP
-	hdrip.ihl =;
-	hdrip.version = ;
-	hdrip.protocol =;
-	hdrip.saddr = ;
-	hdrip.daddr =  ;
-
+	hdrip.ihl = INET_HDR_LEN;
+	hdrip.version = 4;
+	hdrip.protocol = IPROTO_UDP;
+	hdrip.tot_len = sizeof(struct iphdr)+sizeof(struct udphdr);
+	hdrip.saddr =  INADDR_ANY;
+	hdrip.daddr = dest_ip_addr.s_addr;
+	hdrip.ttl = ttl;
 	//UDP
 	hdrudp.source = ;
 	hdrudp.dest = ;
 
+	struct timeval t;
+	t.tv_sec=T_SEC;
+	t.tv_usec= T_USEC;
 
+	int count=0;
 	while(1)
 	{
-		
-		sendto(udpfd, buffer, sizeof(buffer),  , );
-		r = select(icmpfd,  , , );
-		if(r<0)
+		FD_ZERO(&rset);
+		FD_SET(&icmpfd);
+		sendto(udpfd, buffer, sizeof(buffer),  (const struct sockaddr *)&destaddr , sizeof(destaddr) );
+		// start time
+		t = clock();
+		r = select(icmpfd+1, &rset , 0,0, &t);
+		// end time
+		t = clock()-t;
+		time_taken = (double)t / CLOCKS_PER_SEC;
+
+		if(r<0 && (errno!=EINTR))
 		{
 			perror("error in select function");
 		}
-		else if(r==0)
+
+		if(r==0)
 		{
+			if(count==3)
+			{
+				printf("Hop_Count(TTL Value) %d, * *\n", ttl);
+			}
+			count++;
 			t.tv_sec = T_SEC;
 			t.tv_usec = T_USEC;
 		}
-		else
+		if(FD_ISSET(icmpfd, &rset))
 		{
-			DEST_UNREACHABLE
-			break;
+			len = sizeof(checkaddr);
+			ret = recvfrom(icmpfd, icmp_buffer, sizeof(icmp_buffer), 0, (struct sockaddr*)&checkaddr, &len);
+			icmp_hdrip = (struct iphdr*)icmp_buffer;
+
+			hdricmp = (struct icmphdr*)(icmp_buffer + sizeof(struct iphdr));
+
+			if(hdricmp.type == 3)
+			{ // DEST_UNREACHABLE
+				if(checkaddr == destaddr)
+				{
+					printf("Hop_Count(TTL Value) %d, IP_Address %s Response_time %f\n", ttl , inet_ntoa(checkaddr.sin_addr), time_taken );
+					break;
+				}
+				else
+				{
+					printf("not reached correct dest\n");
+				}
+			}
+			else if(hdricmp.type ==11)
+			{//TIME EXCEEDED
+				printf("Hop_Count(TTL Value) %d, IP_Address %s Response_time %f\n", ttl , inet_ntoa(checkaddr.sin_addr), time_taken );
+			}
+			// ignore all other types of icmp packets
 		}
 
-		hdrip.ttl++;
+		ttl++;
+		hdrip.ttl = ttl;
 	}
 
+	close(icmpfd);
+	close(udpfd);
 
 }
